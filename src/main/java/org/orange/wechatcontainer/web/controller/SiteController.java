eb1cd351dc943561ce666c8abcbd9fde119659b4
ornagejyc <ccloveni@163.com>
2014-07-18 02:43:57 +0800

    add admincontroller and sitcontroller

---
+++ src/main/java/org/orange/wechatcontainer/web/controller/SiteController.java
@@ -0,0 +1,67 @@
+/*
+ * 用于前端展示站分发
+ */
+package org.orange.wechatcontainer.web.controller;
+
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.Charset;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletInputStream;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.StreamUtils;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+import org.orange.wechatcontainer.pojo.CardInfo;
+import org.orange.wechatcontainer.sdk.bean.EventMessage;
+import org.orange.wechatcontainer.sdk.bean.xmlmessage.XMLTextMessage;
+import org.orange.wechatcontainer.sdk.ext.RequestContext;
+import org.orange.wechatcontainer.sdk.ext.RequestContextImpl;
+import org.orange.wechatcontainer.sdk.ext.SignatureInfo;
+import org.orange.wechatcontainer.sdk.util.SignatureUtil;
+import org.orange.wechatcontainer.sdk.util.XMLConverUtil;
+import org.orange.wechatcontainer.service.CardInfoService;
+import org.orange.wechatcontainer.service.TentantInfoService;
+
+
+@Controller
+@RequestMapping("/site")
+public class SiteController extends BaseSpringController{
+
+	private static final String CARD_INFO_PAGE="site/cardinfo";
+	
+	@Autowired
+	private CardInfoService cardInfoService=null;
+	
+	
+	public CardInfoService getCardInfoService() {
+		return cardInfoService;
+	}
+
+
+	public void setCardInfoService(CardInfoService cardInfoService) {
+		this.cardInfoService = cardInfoService;
+	}
+
+
+	@RequestMapping(value="/{cardId}",method=RequestMethod.GET)
+	public ModelAndView showCard(@PathVariable String cardId){
+		CardInfo card=(CardInfo)cardInfoService.getFullCardInfoById(cardId);
+		ModelAndView mv=new ModelAndView(CARD_INFO_PAGE);
+		mv.addObject("cardinfo", card);
+		return mv;
+	}
+}
