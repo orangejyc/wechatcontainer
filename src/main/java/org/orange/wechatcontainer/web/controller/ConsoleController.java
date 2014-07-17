28a8e80b8e1806176d6e1dfd07d79bca91849661
ornagejyc <ccloveni@163.com>
2014-07-18 02:50:38 +0800

    add console part

---
+++ src/main/java/org/orange/wechatcontainer/web/controller/ConsoleController.java
@@ -0,0 +1,49 @@
+/*
+ * 用户微会员商家设置端分发
+ */
+
+package org.orange.wechatcontainer.web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import org.orange.wechatcontainer.pojo.CardInfo;
+import org.orange.wechatcontainer.service.CardInfoService;
+
+
+/**
+ * 
+ * @author orange
+ *
+ */
+@Controller
+@RequestMapping("/console")
+public class ConsoleController extends BaseSpringController{
+
+	private static final String CARD_INFO_PAGE="admin/index";
+	
+	@Autowired
+	private CardInfoService cardInfoService=null;
+	
+	
+	public CardInfoService getCardInfoService() {
+		return cardInfoService;
+	}
+
+
+	public void setCardInfoService(CardInfoService cardInfoService) {
+		this.cardInfoService = cardInfoService;
+	}
+
+
+	@RequestMapping(value="/{cardId}",method=RequestMethod.GET)
+	public ModelAndView showCard(@PathVariable String cardId){
+		CardInfo card=(CardInfo)cardInfoService.getFullCardInfoById(cardId);
+		ModelAndView mv=new ModelAndView(CARD_INFO_PAGE);
+		mv.addObject("cardinfo", card);
+		return mv;
+	}
+}
